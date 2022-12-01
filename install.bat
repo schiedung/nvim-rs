07e9250f7d38f26582c4381778e19da2967f0c9d
Raphael Schiedung <schiedung@openphase-solutions.com>
2022-12-01 12:00:53 +0100

    Added windows vim install

--- install.bat
+++ install.bat
@@ -3,4 +3,12 @@ MKDIR %HOMEPATH%\AppData\Local\nvim
 COPY %cd%\nvim_win\init.vim %HOMEPATH%\AppData\Local\nvim
 git clone https://github.com/VundleVim/Vundle.vim.git %HOMEPATH%\AppData\Local\nvim\bundle\Vundle.vim
 nvim +VundleInstall +qall
-git config --global core.editor "nvim"
+ECHO Installing nvim configuration
+MKDIR %HOMEPATH%\vim
+COPY %cd%\vim_win\vimrc %HOMEPATH%\.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git %HOMEPATH%\AppData\Local\vim\bundle\Vundle.vim
+vim +VundleInstall +qall
+ECHO Configure git
+git config --global core.editor nvim
+git config --global merge.tool vimdiff
+git config --global diff.tool vimdiff
--- purge.bat
+++ purge.bat
@@ -1,2 +1,5 @@
 ECHO Uninstalling nvim configuration
 DEL %HOMEPATH%\AppData\Local\nvim
+ECHO Uninstalling vim configuration
+DEL %HOMEPATH%\vim
+DEL %HOMEPATH%\.vimrc
