e66980594dd93893501feb61d8628a9c6ef263cc
Raphael Schiedung <raphael.schiedung@gmail.com>
2024-10-06 14:11:03 +0200

    Added telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -260,6 +260,16 @@ require("lazy").setup({
         }
       end,
     },
+    {'nvim-telescope/telescope.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      end,
+    },
   },
 
   -- Configure any other settings here. See the documentation for more details.
