4136b6ea787516c0af5aa0be22026ae5e9a2f9ae
Raphael Schiedung <raphael.schiedung@gmail.com>
2024-11-04 14:11:49 +0100

    Set plugin update to auto

--- nvim/init.lua
+++ nvim/init.lua
@@ -279,5 +279,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    auto_install = true,
+  },
 })
