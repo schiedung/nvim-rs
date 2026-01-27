2e469f1e616527a47c66623b29a6c44a5ef84f99
Raphael Schiedung <raphael.schiedung@gmail.com>
2026-01-27 10:13:57 +0100

    Fixed leader keybinding

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,8 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
 vim.opt.number = true -- Add line numbers
 vim.opt.relativenumber = true  -- Set relative numbers
